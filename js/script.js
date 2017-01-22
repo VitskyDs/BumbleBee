0efb346721142a21b296dd5c2e1fb90105d61d1b
Vitsky <vitskyds@gmail.com>
2017-01-22 22:19:21 +0200

    text animation 0.1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 
 var queenB = $('#QueenB'),
     i = 0,
-    menuTmln = new TimelineLite(),
+    menuTmln = new TimelineMax(),
     line1 = $("#line-1"),
     line2 = $("#line-2"),
     line3 = $("#line-3"),
     menuContainer = $(".menu-container"),
-    tmln = new TimelineLite(),
-    txtTmln = new TimelineLite();
+    tmln = new TimelineMax(),
+    txtTmln = new TimelineMax({repeat: -1, repeatDelay:1});
 
 CustomEase.create("queenB", "M0,0 C0.502,0 0.479,0.611 0.588,0.816 0.686,1 0.818,1 1,1");
 
@@ -59,19 +59,21 @@ $(".menu").click(function(){
 
 //bumbleType animation
 
-txtTmln .set(".text-animation", {autoAlpha: 0}).set("#B1", {color: "#000"}).set("#e0", {css:{display:"none"}})
-        .to("#B0", 0, {autoAlpha:1, delay:0.3})
+txtTmln .set(".text-animation", {autoAlpha: 0}).set("#B1", {color: "#f64870"}).set("#e0", {css:{display:"none"}})
+        .to("#B0", 0, {autoAlpha:1, delay:0.8})
         .to("#u", 0, {autoAlpha:1, delay:0.3})
         .to("#m", 0, {autoAlpha:1, delay:0.3})
         .to("#b", 0, {autoAlpha:1, delay:0.3})
-        .to("#l", 0, {autoAlpha:1, delay:0.2})
-        .to("#B1", 0, {autoAlpha:1, delay:0.2})
-        .to("#B1", 0, {css:{display:"none"}, delay:0.4})
+        .to("#l", 0, {autoAlpha:1, delay:0.3})
+        .to("#B1", 0, {autoAlpha:1, delay:0.3})
+        .to("#B1", 0, {css:{display:"none"}, delay:0.8})
         .to("#e0", 0, {css:{display:"inline"}})
-        .to("#e0", 0, {autoAlpha:1, delay:0.6})
+        .to("#e0", 0, {autoAlpha:1, delay:0.1})
         .to("#B2", 0, {autoAlpha:1, color:"#fff", delay:0.3})
-        .to("#e1", 0, {autoAlpha:1, delay:0.2})
-        .to("#e2", 0, {autoAlpha:1, delay:0.2});
+        .to("#e1", 0, {autoAlpha:1, delay:0.3})
+        .to("#e2", 0, {autoAlpha:1, delay:0.3})
+        .to("#bumble-type", 0.3, {ease: Sine.easeIn, className:"+=glow"})
+;
 
 $(window).click(function() {
 menuAnimationReverse();
